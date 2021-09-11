521bb97e9a3d9fc066784dc6ce9f167a9f1a81f2
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-09-11 21:24:01 +0300

    add event search feature

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+const refs = {
+    eventInput: document.querySelector('.search-event-area'),
+    countryInput: document.querySelector('.search-country-area'),
+    gallery: document.querySelector('.gallery'),
+  };
+  
+  export const {eventInput, countryInput, gallery} = refs;
+
+  
