ba5be7d828c8ce1c10b3807ae6a81012bc148a05
Ihor Rudniev <rudnievihor@gmail.com>
2021-09-12 08:17:40 +0300

    Update event_search.js

---
+++ src/js/event_search.js
@@ -0,0 +1,20 @@
+import fetchEventsByName from './api_service';
+import { eventInput, countryInput, gallery } from './refs';
+import { createGalleryMarkup } from './create-markup';
+
+eventInput.addEventListener('input', onEventSearch);
+
+const state = {
+  page: 1,
+  query: '',
+};
+
+async function onEventSearch(e) {
+  state.page = 1;
+  // if (!e.currentTarget.elements.query.value.trim()) {
+  //   return;
+  // }
+  state.query = eventInput.value.trim();
+  const data = await fetchEvents(state.query, state.page);
+  createGalleryMarkup(data);
+}
