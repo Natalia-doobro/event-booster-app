2fb36267a43c4e35c27f52c8b93641790f5bf39b
Andrew Galkin <andrushka@MacBook-Air-Andrew.local>
2021-09-13 18:42:37 +0300

    update structure

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,9 +1,13 @@
 const refs = {
-    eventInput: document.querySelector('.search-event-area'),
-    countryInput: document.querySelector('.search-country-area'),
-    gallery: document.querySelector('.gallery'),
-  };
-  
+  eventInput: document.querySelector('.search-event-area'),
+  countryInput: document.querySelector('.search-country-area'),
+  gallery: document.querySelector('.gallery'),
+  closeModal: document.querySelector('.close-modal'),
+  modalOverlay: document.querySelector('.modal-overlay'),
+  modalImg: document.querySelector('.img-card_image'),
+  smallModalImg: document.querySelector('.img-card_image_small'),
+  modalGallery: document.querySelector('.modal_gallery'),
+};
+
   export const {eventInput, countryInput, gallery} = refs;
 
-  
