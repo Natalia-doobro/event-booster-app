d1737d5352193be35ccd938f61e45a89107dcddf
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-09-14 23:26:09 +0300

    fix pnotify bugs

--- src/js/api_service.js
+++ src/js/api_service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
 
 export async function fetchEventsByName(query, page){
-  const {data: {_embedded: {events}}} = await axios.get(`size=20&keyword=${query}&page=${page}&apikey=1twKLyrauG3OZrFZiN9ApTE1ANWFyZTo`)
+  const {data: {_embedded: {events}}} = await axios.get(`size=20&keyword=${query}&page=${page}&sort=random&apikey=1twKLyrauG3OZrFZiN9ApTE1ANWFyZTo`)
   return events; 
 }
 
