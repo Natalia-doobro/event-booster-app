247edb35b481e640fd85e8d276ee4b182356f293
Ihor Rudniev <rudnievihor@gmail.com>
2021-09-16 08:59:20 +0300

    add-render-countries-list

--- src/js/create-markup.js
+++ src/js/create-markup.js
@@ -1,12 +1,21 @@
 import galleryMarkUp from '../templation/gallery.hbs';
-import {eventInput, countryInput, gallery} from './refs';
+import { eventInput, countryInput, gallery } from './refs';
 
 export function createGalleryMarkup(events) {
-    const markUp = galleryMarkUp(events);
-    gallery.insertAdjacentHTML('beforeend', markUp);
-  }
-  
+  const markUp = galleryMarkUp(events);
+  // console.log(markUp);
+  gallery.insertAdjacentHTML('beforeend', markUp);
+}
 
-  export function clearGalleryMarkup() {  
-    gallery.innerHTML = '';
-  } 
+export function clearGalleryMarkup() {
+  gallery.innerHTML = '';
+}
+
+//=== рендер списка стран второго инпута ============
+
+export function createCountriesListMarkup(events) {
+  const countriesMarkup = countriesListMarkup(events);
+  console.log(countriesMarkup);
+
+  countryInput.insertAdjacentHTML('beforeend', countriesMarkup);
+}
