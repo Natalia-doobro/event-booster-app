4798713c86aa5f985b0e1bd8ec7434f257e2d5dd
ermak-yana <lidl.peperts.stor@gmail.com>
2021-09-18 16:30:28 +0300

    Team-modal-fix

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,13 +1,11 @@
 import teamModalTpl from '../templation/teamModal.hbs';
 import teamData from '../data/team.js';
 
-// $('.hover').mouseleave(function () {
-//   $(this).removeClass('hover');
-// });
+
 
 const openTeamModal = document.querySelector('.footer-link');
 const teamModal = document.querySelector('.modal-team');
-// const scrollUp = document.querySelector('.scroll-up');
+
 
 openTeamModal.addEventListener('click', e => {
   e.preventDefault();
@@ -17,13 +15,13 @@ openTeamModal.addEventListener('click', e => {
 
   const btnCloseModal = document.querySelector('.close');
   document.body.style.overflow = 'hidden';
-//   scrollUp.style.display = 'none';
+
   teamModal.classList.remove('is-hidden');
   teamModal.classList.add('is-open');
 
   btnCloseModal.addEventListener('click', () => {
     document.body.style.overflow = 'visible';
-    // scrollUp.style.display = 'block';
+   
     teamModal.classList.remove('is-open');
     teamModal.classList.add('is-hidden');
   });
