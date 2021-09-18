302f08cf797c7619785234a3a734a7a395fb1ba5
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-09-18 11:58:32 +0300

    change api fetch functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import  Pagination from './js/pagination.js';
 import './sass/main.scss';
-import fetchEventsByName from './js/event_search';
+import {onLoadPage, onEventSearch} from './js/event_search';
 import './js/header-animation.js';
