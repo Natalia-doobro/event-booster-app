7db419bbb8df66c31a2cef8fe8939caf15847efe
Ihor Rudniev <rudnievihor@gmail.com>
2021-09-19 11:18:00 +0300

    update-search-country

--- src/js/event_search_country.js
+++ src/js/event_search_country.js
@@ -2,13 +2,27 @@ import { countryInput } from './refs';
 import { clearGalleryMarkup, createGalleryMarkup } from './create-markup';
 import { state } from './event_search';
 import countriesList from '../data_countries.json';
+import listCountriesTpl from '../templation/list-countries.hbs';
 
-countryInput.addEventListener('input', onCountrytSearch);
+console.log(listCountriesTpl(countriesList[10]));
+
+countryInput.addEventListener('input', debounce(onCountrytSearch, 1000));
+
+function inputHandler() {
+  const keys = Object.keys(countriesList);
+  const country = keys.find(key => {
+    if (key.includes(countryInput.value)) {
+      return key;
+    }
+  });
+
+  state.code = countriesList[country];
+  return state.code;
+}
 
 export async function onCountrytSearch(e) {
-  // state.code = e.target.value.trim();
-  // if(countriesList.key.includes(e))
-  const data = await fetchCountries(state.page, state.code);
+  inputHandler();
+  const data = await fetchEvents(state.query, state.page, state.classification, state.code);
   clearGalleryMarkup();
   createGalleryMarkup(data);
 }
