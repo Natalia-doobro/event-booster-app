2c1758de8ab9beed60795c6979b770e4a7d72353
Natalia-doobro <79019782+Natalia-doobro@users.noreply.github.com>
2021-09-19 17:42:16 +0300

    правочки

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Pagination from './js/pagination.js';
 import './sass/main.scss';
 import { onLoadPage, onEventSearch } from './js/event_search';
 import './js/header-animation.js';
-import { onCountrytSearch } from './js/event_search_country.js';
+import { onCountrytSearch } from './js/event_search_country';
 import fetchEventsByName from './js/event_search';
 import './data/team.js';
 import './js/team-modal';
