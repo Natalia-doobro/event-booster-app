c8fb225676a875ecab22ba704066612b98dc37f2
Natalia-doobro <79019782+Natalia-doobro@users.noreply.github.com>
2021-09-20 08:23:02 +0300

    fix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -5,42 +5,41 @@ export const container = document.getElementById('tui-pagination-container');
 
 export const options = {
     totalItems: 500,
-    itemsPerPage: 10,   
+    itemsPerPage: 10,
     visiblePages: 5,
     page: 1,
     centerAlign: true,
 };
 
-export const myPagination = new Pagination (container, options);
+export const myPagination = new Pagination(container, options);
 
 myPagination.on('afterMove', (event) => {
     const currentPage = event.page;
-    
-    
-    
-        if (currentPage === state.page + 1) {
-           loadNextPage();
-        }
-        if (currentPage === state.page - 1) {        
-            loadPrevPage();
-        }
-    
-        else {
-           state.page = `${currentPage}`;  
-            onLoadPage();     
-        }
 
+
+
+    if (currentPage === state.page + 1) {
+        loadNextPage();
     }
+    if (currentPage === state.page - 1) {
+        loadPrevPage();
+    }
+
+    else {
+        state.page = `${currentPage}`;
+        onLoadPage();
+    }
+
 });
 
-    function incrementPage() {
-        state.page++;
-    }
-    
-       
-    function dicrementPage() {
-        state.page--;
-    }
+function incrementPage() {
+    state.page++;
+}
+
+
+function dicrementPage() {
+    state.page--;
+}
 
 
 
